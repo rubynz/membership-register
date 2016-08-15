0eaa07db461fb3e0ac3ec0c539b80543973897f3
Daniel Fone <daniel@fone.net.nz>
2016-08-15 22:25:21 +1200

    Add uniqueness validations

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,5 +1,7 @@
 class Member < ApplicationRecord
   validates_presence_of :full_name, :email, :address
+  validates_uniqueness_of :full_name, if: :full_name_changed?
+  validates_uniqueness_of :email, if: :email_changed?
 
   before_create :set_joined_at
 
