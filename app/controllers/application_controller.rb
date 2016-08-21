97701611eeed3a2159899e4cb3c576017fd53f22
Daniel Fone <daniel@fone.net.nz>
2016-08-21 21:51:54 +1200

    Setup mailer config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   responders :flash
+
+  before_action :set_mailer_host
+
+private
+
+  def set_mailer_host
+    ActionMailer::Base.default_url_options = {
+      host: request.host_with_port
+    }
+  end
 end
