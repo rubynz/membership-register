a8dba96091b95e850c701d33538061e15cc6afa6
Daniel Fone <daniel@fone.net.nz>
2016-08-21 22:27:14 +1200

    Fix SMTP config

--- config/initializers/email_delivery.rb
+++ config/initializers/email_delivery.rb
@@ -1,9 +1,17 @@
-ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
-  :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'heroku.com',
-  :enable_starttls_auto => true
-}
+smtp_username = ENV['SENDGRID_USERNAME']
+smtp_password = ENV['SENDGRID_PASSWORD']
+
+if smtp_username && smtp_password
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => smtp_username,
+    :password       => smtp_password,
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+else
+  warn "** SKIPPING SMTP CONFIGURATION"
+end
