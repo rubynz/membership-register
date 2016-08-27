6448af79481e15ec2fd2a13e8e3c931762e9713d
Daniel Fone <daniel@fone.net.nz>
2016-08-28 00:19:23 +1200

    Fix admin membership management

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -6,10 +6,25 @@ module Admin
       @members = Member.all.order(joined_at: :desc)
     end
 
+    def create
+      @member = Member.new(member_params)
+      @member.save
+      respond_with :admin, @member
+    end
+
+    def update
+      @member.update(member_params)
+      respond_with :admin, @member
+    end
+
   private
 
     def token_valid?
       true
+    end
+
+    def member_params
+      params.require(:member).permit(:full_name, :email, :address, :joined_at)
     end
 
   end
