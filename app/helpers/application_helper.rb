52d7f15c656513cceb07b550dbe0a4eda89ced96
Daniel Fone <daniel@fone.net.nz>
2016-08-28 01:33:06 +1200

    Add updated time on members index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+
   def render_attribute(model, attr, &block)
     attr_name = model.class.human_attribute_name(attr)
     value = block_given? ? capture(&block) : model[attr]
@@ -10,4 +11,11 @@ module ApplicationHelper
       ], ' '
     end
   end
+
+  def time_ago_span(time)
+    return unless time
+
+    content_tag(:span, time_ago_in_words(time), title: time)
+  end
+
 end
