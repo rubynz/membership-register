8533af601f4980a79dee73656240995a668f9136
Pete Nicholls <pn@trineo.com>
2020-05-10 13:21:16 +1200

    Upgrade to Rails 5.1
    This change was performed by running `rails app:update` and reviewing
    the changes suggested to bring this app up to the 5.1 defaults.
    
    Notes:
    
    * HSTS on subdomains is now a default, and does not need to be
    explicitly set
    * CoffeeScript support has been removed in favour of plain JavaScript
    * pry has been removed in favour of the Rails default byebug

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      'Cache-Control' => 'public, max-age=172800'
+      'Cache-Control' => "public, max-age=#{2.days.seconds.to_i}"
     }
   else
     config.action_controller.perform_caching = false
@@ -27,10 +27,9 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.delivery_method = :letter_opener
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -52,4 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Preview emails in development
+  config.action_mailer.delivery_method = :letter_opener
 end
