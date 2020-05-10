e89dfdf08504ca6aa3a44d326bb5c0de6398c12c
Pete Nicholls <pn@trineo.com>
2020-05-10 13:11:00 +1200

    Use Rails 5.0 defaults
    This change was performed by running `rails app:update` and reviewing
    the changes suggested to bring this app up to the 5.0 standards.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,6 +38,9 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
 
+  # Enable HTTP Strict Transport Security for subdomains
+  config.ssl_options = { hsts: { subdomains: true } }
+
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
   config.log_level = :debug
