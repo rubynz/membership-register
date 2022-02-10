a8b1f74332037da815c4d45205cbb59b07b6f360
Sarah Hay <shay@tractionondemand.com>
2022-02-10 17:11:22 +1300

    Correct formatting of confirmation email
    Adds break between valediction and signature.
    Removes full stop from email signature.

--- spec/features/membership_confirmation_spec.rb
+++ spec/features/membership_confirmation_spec.rb
@@ -67,7 +67,7 @@ RSpec.describe "membership confirmation process" do
           (?-x:next month, we will remove your details from the Ruby New Zealand).*
           (?-x:Member’s register. Should you wish to rejoin, you can register).*
           (?-x:for membership here:).*
-          (?-x:Kind regards.*The 2021 New Zealand Ruby Committee.)
+          (?-x:Kind regards.*The 2021 New Zealand Ruby Committee)
         }xm
       )
 
